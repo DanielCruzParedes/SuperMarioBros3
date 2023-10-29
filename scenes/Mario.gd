0b75454caf6a80aa27f6ec5f980f8ae97fc85a52
Daniel Cruz <danielcruzp@unitec.edu>
2023-10-28 18:33:16 -0600

    Animaciones puestas al mario pequeño,
    El boton de saltar ahora es la z, tambien le puse collision boxes a todo el nivel

--- scenes/Mario.gd
+++ scenes/Mario.gd
@@ -1,43 +1,71 @@
 extends CharacterBody2D
 
-const SPEED = 120.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -375.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var salto_valido=false;
 var tiempo_salto=0.0
+var colision
 
 func _physics_process(delta):
 	detectar()
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
+	velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("saltar") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		tiempo_salto=0.0
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("izquierda", "derecha")
-	if direction:
-		velocity.x = direction * SPEED
+	#if Input.is_action_pressed("derecha") or Input.is_action_pressed("izquierda"): 
+	velocity.x = direction * SPEED
+	if Input.is_action_pressed("derecha"):
+		
+		$SpriteMario.flip_h = false
+		if is_on_floor():
+			$Animacion.play("run")
+			
+	elif Input.is_action_pressed("izquierda"):
+		
+		$SpriteMario.flip_h = true
+		if is_on_floor():
+			$Animacion.play("run")
 	else:
-		if Input.is_action_just_pressed("saltar") and salto_valido:
-			if tiempo_salto<0.2:
-				velocity.y=+JUMP_VELOCITY
-				tiempo_salto+=delta
-				salto_valido=false
-		elif Input.is_action_just_released("saltar") and velocity.y<-150:
-			velocity.y +=100
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if salto_valido:
+			$Animacion.play("idle")
+	
+	if Input.is_action_pressed("speed") and Input.is_action_pressed("derecha") and salto_valido:
+			velocity.x = SPEED+75
+			$Animacion.speed_scale = 2
+	elif Input.is_action_pressed("speed") and Input.is_action_pressed("izquierda") and salto_valido:
+			velocity.x = (SPEED*-1)-75
+			$Animacion.speed_scale = 2
+	else:
+		$Animacion.speed_scale = 1
 	move_and_slide()
+	
+	if Input.is_key_pressed(KEY_Z) and not is_on_floor():
+		$Animacion.play("jump")
+		
+	if Input.is_action_just_pressed("saltar"):
+		if salto_valido:
+			$Animacion.play("jump")
+			velocity.y=+JUMP_VELOCITY
+			salto_valido=false
+			
+	elif Input.is_action_just_released("saltar") and salto_valido:
+		if salto_valido:
+			$Animacion.play("jump")
+			velocity.y +=200
+			salto_valido=false
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
+	
 	
 func detectar():
 		if $abajo.is_colliding():
-			var colision=$abajo.get_collider()
+			colision=$abajo.get_collider()
 			if colision.is_in_group("suelos"):
 				salto_valido=true
 				
