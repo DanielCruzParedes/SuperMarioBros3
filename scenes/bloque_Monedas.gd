558c4eed79dc8c5bc7f0f94b712e7e4d6359e987
AzaliaFlores19 <yamiflo19@unitec.edu>
2023-10-30 00:18:08 -0600

    -Se arreglo la aparicion del hongo y bloques bloqueados

--- scenes/bloque_Monedas.gd
+++ scenes/bloque_Monedas.gd
@@ -1,14 +1,16 @@
 extends StaticBody2D
-
+var puede_moverse=true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 func muevete():
-	position.y-=3
-	$activado.visible=false
-	await(get_tree().create_timer(0.12).timeout)
-	position.y+=3
+	if puede_moverse:
+		puede_moverse=false
+		position.y-=3
+		$activado.visible=false
+		await(get_tree().create_timer(0.12).timeout)
+		position.y+=3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- scenes/bloques/bloque_Power.gd
+++ scenes/bloques/bloque_Power.gd
@@ -1,23 +1,28 @@
 extends StaticBody2D
-
+var hongo_instanciado = false
+var puede_moverse=true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 func muevete():
-	position.y-=3
-	$activado.visible=false
-	await(get_tree().create_timer(0.12).timeout)
-	position.y+=3
-	instanciar()
+	if puede_moverse:
+		puede_moverse=false
+		position.y-=3
+		$activado.visible=false
+		await(get_tree().create_timer(0.12).timeout)
+		position.y+=3
+		instanciar()
 
 func instanciar():
-	var hongo= preload("res://scenes/Power_Ups/hongo.tscn")
-	print("Hongo cargado")
-	var hongo_instance=hongo.instantiate()
-	hongo_instance.global_position= $spawn_hongo.global_position
+	if not hongo_instanciado:
+		var hongo= preload("res://scenes/Power_Ups/hongo.tscn")
+		print("Hongo cargado")
+		var hongo_instance=hongo.instantiate()
+		hongo_instance.global_position= $spawn_hongo.global_position
+		get_tree().root.add_child(hongo_instance)
+		hongo_instanciado = true
 	
-	get_tree().root.add_child(hongo_instance)
 	
 
 
