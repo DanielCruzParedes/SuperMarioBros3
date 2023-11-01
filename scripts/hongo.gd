33f300d619b2e7cfcbf5694709b805118d71e4e9
Daniel Cruz <danielcruzp@unitec.edu>
2023-10-31 19:02:45 -0600

    Salido del hongo del bloque terminado

--- scripts/hongo.gd
+++ scripts/hongo.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
-#PORFA SERVIIIIIIIIIIIIII
+
 
 const SPEED = 50.0
 var pegoDerecha=false
 var pegoIzquierda=false
+var puedoMoverme = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,21 +13,28 @@ func muevete():
 	pass
 
 func _physics_process(delta):
-	position.y  -= 5 
-	detectar()
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if puedoMoverme == true:
+		detectar()
+		
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			move_and_slide()
+		if pegoDerecha==false and pegoIzquierda==false:
+			velocity.x = SPEED
+			move_and_slide()
+		elif pegoDerecha==true:
+			velocity.x = -SPEED
+			move_and_slide()
+		elif pegoIzquierda==true:
+			velocity.x = SPEED
+			move_and_slide()
+	else:
+		position.y -= 1
 		move_and_slide()
-	if pegoDerecha==false and pegoIzquierda==false:
-		velocity.x = SPEED
-		move_and_slide()
-	elif pegoDerecha==true:
-		velocity.x = -SPEED
-		move_and_slide()
-	elif pegoIzquierda==true:
-		velocity.x = SPEED
-		move_and_slide()
+		await get_tree().create_timer(0.25).timeout
+		puedoMoverme=true
+		$colisionHongo.disabled=false
+		
 
 func detectar():
 		if $derecha.is_colliding():
